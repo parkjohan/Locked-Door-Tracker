07c6597efe2072e1da5c3a2ee19f457cc97587fd
Johan Park <parkjohan@gmail.com>
2022-04-27 20:08:20 -0400

    Initial Commit

---
+++ Locked Door Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locked Door Tracker
+//
+//  Created by Johan Park on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Locked Door Tracker/Locked_Door_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Locked_Door_TrackerApp.swift
+//  Locked Door Tracker
+//
+//  Created by Johan Park on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Locked_Door_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
